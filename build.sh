378defa58da2a1724301256ef72654c13c206dad
JoeDIOP <diop_joseph@outlook.fr>
2026-01-31 00:40:26 +0100

    Add auto createsuperuser for Render free plan

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
-
-#!/bin/bash
+#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
-python manage.py migrate
+python manage.py migrate --noinput
+
+# Création automatique du superuser si demandé
+if [ "$CREATE_SUPERUSER" = "True" ]; then
+  echo "Creating Django superuser..."
+  python manage.py createsuperuser --no-input || true
+else
+  echo "Skipping superuser creation."
+fi
