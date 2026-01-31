ecc58dd3cb005cb90900aff57b78ca21eb00e2e7
JoeDIOP <diop_joseph@outlook.fr>
2026-01-31 18:17:24 +0100

    Add files via upload

--- adhesions/admin.py
+++ adhesions/admin.py
@@ -3,7 +3,7 @@ from .models import Adhesion
 
 @admin.register(Adhesion)
 class AdhesionAdmin(admin.ModelAdmin):
-    list_display = ('nom_prenom', 'email', 'village_quartier', 'domaine_participation', 'date_inscription', 'statut')
+    list_display = ('nom_prenom', 'email', 'telephone', 'village_quartier', 'domaine_participation', 'date_inscription', 'statut')
     list_filter = ('domaine_participation', 'statut', 'date_inscription')
     search_fields = ('nom_prenom', 'email', 'village_quartier')
     ordering = ('-date_inscription',)
