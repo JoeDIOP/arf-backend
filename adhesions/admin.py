073e06f967aad7edf11e6acbe74b4849a9b070cc
JoeDIOP <diop_joseph@outlook.fr>
2026-01-31 14:23:44 +0100

    Add files via upload

--- adhesions/admin.py
+++ adhesions/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Adhesion
 
-# Register your models here.
+@admin.register(Adhesion)
+class AdhesionAdmin(admin.ModelAdmin):
+    list_display = ('nom_prenom', 'email', 'village_quartier', 'domaine_participation', 'date_inscription', 'statut')
+    list_filter = ('domaine_participation', 'statut', 'date_inscription')
+    search_fields = ('nom_prenom', 'email', 'village_quartier')
+    ordering = ('-date_inscription',)
+    readonly_fields = ('date_inscription',)
+    fieldsets = (
+        (None, {
+            'fields': ('nom_prenom', 'email', 'telephone', 'village_quartier', 'domaine_participation', 'statut')
+        }),
+        ('Dates', {
+            'fields': ('date_inscription',),
+            'classes': ('collapse',),
+        }),
+    )
